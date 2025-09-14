a3e4544ff15fcf34a0dab445ead84b840a75a0da
2nayeon <leenayeon0915@gmail.com>
2025-09-15 00:12:48 +0900

    week02: README.md & variables v0.2

--- KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -27,7 +27,22 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        week02Variable()
     }
+}
+
+fun week02Variable(){
+    println("Week02 Variables")
+
+    val courseName = "Mobile Programming"
+    // courseName = "IoT Programming" -> val는 상수선언이라 내용 변경 못함.
+
+    var week = 1
+    week = 2
+    println("Course: $courseName")
+    // $courseName -> 문자열 템플릿
+    // 문자열 템플릿: 문자열 안에 변수를 쉽게 넣기 위해 $변수명 형식을 사용함.
+    println("Week: $week")
 }
 
 @Composable
